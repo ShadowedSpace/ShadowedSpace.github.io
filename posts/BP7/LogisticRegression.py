24827d6349a06691731706f185efe8e0653e67e1
Zoe Greenwald <zgreenwald@middlebury.edu>
2024-04-23 08:40:32 -0400

    LR graphing issues

--- posts/BP7/LogisticRegression.py
+++ posts/BP7/LogisticRegression.py
@@ -6,7 +6,7 @@ class LinearModel:
 
     def score(self, X):
         return X@self.w
-    
+
     def predict(self, X):
         return self.score(X) > 0
 
@@ -14,30 +14,36 @@ class LogisticRegression(LinearModel):
     def __init__(self, w):
         self.w = w
 
-    def sigmoid(s):
-        return 1 / (1 + torch.exp(s))
+    def sigmoid(self, s):
+        return 1 / (1 + torch.exp(-s))
 
     def loss(self, X, y):
         s = self.score(X)
         sigma = self.sigmoid(s)
 
-        first_half = sigma.log()*y
-        second_half = (1 - sigma).log() * (1 - y)
+        first_half = -sigma.log()*y
+        second_half = -(1 - sigma).log() * (1 - y)
 
-        return (1 / X.size) * (first_half + second_half).sum()
-    
+        return (1 / X.size(0)) * (first_half + second_half).sum()
+
     def grad(self, X, y):
-        # v_ = v[:, None]
         s = self.score(X)
         sigma = self.sigmoid(s)
+        sigma = sigma[:, None]
 
-        return (1 / X.size) * (sigma - y) * X
-    
-class LogisticOptimizer:
-    def __init__(self, model, w):
+        y = y[:, None]
+
+        grad = (sigma - y) * X
+
+        return grad.mean(dim=0)
+
+class GradientDescentOptimizer:
+    def __init__(self, model, w, w_prev):
         self.model = model 
-        self.w = None
-        self.w_prev = None
+        self.w = w
+        self.w_prev = w_prev
 
     def step(self, X, y, alpha, beta):
-        return self.w - alpha * (self.model.grad(X, y)) + beta * (self.w - self.w_prev)
+        temp = self.model.w - alpha * (self.model.grad(X, y)) + beta * (self.model.w - self.w_prev)
+        self.w_prev = self.model.w
+        self.model.w = temp
